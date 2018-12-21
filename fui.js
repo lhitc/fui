8ea8662209883dab5c247355074e6ba794bddb43
Stefan Lang <stefan.lang@lang-hitc.de>
2018-12-21 17:13:16 +0100

    Metadaten ausgeben

--- fui.js
+++ fui.js
@@ -15,3 +15,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
+
+
+var Fui = Vue.extend({
+    data: function() {
+        return {
+            questionnaire: null,
+			errorMessage: null
+        }
+    }
+});
